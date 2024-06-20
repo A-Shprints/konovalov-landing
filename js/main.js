83d0999ee13fd36840d8be06c1eb38fc0b4d52dc
Alexander Shprints <alexander.shprints@gmail.com>
2024-06-20 16:27:20 +0400

    initial

---
+++ js/main.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const swiper = new Swiper(".swiper", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: false,
+    slidesPerView: 3,
+    spaceBetween: 30,
+
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+});
