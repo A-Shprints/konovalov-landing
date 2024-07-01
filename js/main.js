1ed73b8c533147a18c4c73a7165c949b5094cb1d
Alexander Shprints <alexander.shprints@gmail.com>
2024-07-01 10:59:42 +0400

    added mobile version of page, tweaked swiper.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // Бургер
+
+  const burger = document.querySelector(".nav__burger");
+  const navContainer = document.querySelector(".container__nav");
+  const body = document.body;
+  const menuItems = document.querySelectorAll(".menu__link");
+
+  burger.addEventListener("click", () => {
+    navContainer.classList.toggle("nav__burger--clicked");
+    body.classList.toggle("no-scroll");
+  });
+
+  menuItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      navContainer.classList.remove("nav__burger--clicked");
+      body.classList.remove("no-scroll");
+    });
+  });
+
+  // Свайпер
+
   const swiper = new Swiper(".swiper", {
     // Optional parameters
     direction: "horizontal",
     loop: false,
-    slidesPerView: 3,
+    slidesPerView: 1,
     spaceBetween: 30,
 
     // If we need pagination
@@ -17,5 +38,44 @@ document.addEventListener("DOMContentLoaded", function () {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
+
+    // Адаптивные настройки
+    breakpoints: {
+      450: {
+        slidesPerView: 2,
+      },
+      800: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+  // EmailJS - Отправка данных формы
+
+  // Модальное окно
+
+  document
+    .getElementById("privacyPolicyLink")
+    .addEventListener("click", function (event) {
+      event.preventDefault();
+      document.getElementById("myModal").style.display = "block";
+    });
+
+  document
+    .getElementById("closeModalBtn")
+    .addEventListener("click", function () {
+      document.getElementById("myModal").style.display = "none";
+    });
+
+  document
+    .getElementById("closeModalFooterBtn")
+    .addEventListener("click", function () {
+      document.getElementById("myModal").style.display = "none";
+    });
+
+  window.addEventListener("click", function (event) {
+    if (event.target == document.getElementById("myModal")) {
+      document.getElementById("myModal").style.display = "none";
+    }
   });
 });
